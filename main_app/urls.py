c4da7f3b769bce3b376605b7a5e08bcea9c25f22
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-06-22 16:53:28 -0600

    Embed Map

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.conf.urls import url                                                                                                                              
 from . import views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('games/', views.GameList.as_view(), name='game_list'),
     path('games/<int:game_id>/', views.game_detail, name='game_detail'),
+    # path('games/<int:game_id>/game_map', views.game_map, name='game_map'),
+    url(r'map/', views.game_map, name="game_map"),
 ]
