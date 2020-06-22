b395ef67b582895f3521af9b8cbec8d0a5841ee3
Lauren Mengert <laurenmengert@Laurens-MacBook-Pro.local>
2020-06-22 12:59:47 -0600

    implement oauth

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Something went wrong! :( Give it another shot'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
