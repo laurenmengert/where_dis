b9c1481a057b80ced70ef869b78e32a45625cdc1
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-06-22 15:32:33 -0600

    resolved routing error

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('games', views.GameList.as_view(), name='game_list'),
-    path('games/<int:pk>', views.game_detail, name='game_detail'),
+    path('games/', views.GameList.as_view(), name='game_list'),
+    path('games/<int:game_id>/', views.game_detail, name='game_detail'),
 ]
