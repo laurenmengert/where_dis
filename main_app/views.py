1d0cc6edd8b7ff133ebe3c64bb2c596f7fc68279
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-06-23 17:33:15 -0600

    grabs photo metadata

--- main_app/views.py
+++ main_app/views.py
@@ -6,23 +6,28 @@ from .models import GameInstance, Photo
 from GPSPhoto import gpsphoto
 import uuid
 import boto3
+import os, sys
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+import copy
+import piexif
 
 
 # ----------------------CONSTANTS-------------------------- #
 
 
-# S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
-# BUCKET = 'wheredis'
+S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
+BUCKET = 'wheredis'
 
-S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
-BUCKET = 'catcollector'
 
 
 # -----------------------GENERAL--------------------------- #
 
 
 def home(request):
-    return render(request, 'home.html')
+  photo_obj = gpsphoto.getGPSData('/Users/Shawn/code/where_dis/main_app/wireframe.jpg')
+  print('photo_obj:', photo_obj)
+  return render(request, 'home.html')
 
 
 def signup(request):
@@ -69,26 +74,113 @@ def game_map(request, game_id):
 # ------------------------PHOTOS---------------------------- #
 
 
+def save_picture(form_picture):
+    # this function has to do with the module Pillow
+    # PIL
+    # purpose is to save the image as a static asset
+    # 1. generate a random name
+    random_hex = uuid.uuid4().hex[:8]# generates a random integer
+    # grabbing the ext , jpeg, jpg
+    
+    print(form_picture, '<=====form_picture')
+    f_name, f_ext = os.path.splitext(form_picture.name)
+    # => ['jimProfile', 'png']
+    # create the random picture name with the correct extension
+    picture_name = random_hex + f_ext
+    # create the file_path
+    file_path = os.path.join(os.getcwd(), 'main_app/tmp/' + picture_name)
+    print(file_path, '<=======================file_path')
+    
+    # file_path = os.path.join(os.getcwd(), 'main_app/tmp' + picture_name) # print this
+    # Pillow code stars
+    # output_size = (125, 175) # set the size of picture, as tuple
+    # open the file sent from the client
+    i = Image.open(form_picture)
+    # exif = i._exif._loaded_exif
+    # print( i.gps_latitude, 'latitude')
+    
+    exif_dict = piexif.load(i.info["exif"])
+    exif_bytes = piexif.dump(exif_dict)
+    
+    print(exif_bytes, '<=======exif_bytes')
+    
+    # print(exif, 'exifffffffffffffffffffffffffffff')
+    
+    # i.thumbnail(output_size) # set the size accepts a tuple with dimensions
+    i.save(file_path) # save it to file path we created
+    return file_path  #RETURN FILEPATH
+  
+  
+# CREATE HELPER FUNCTION TO DELETE
+
+
 def upload_photo(request, game_id): # DOUBLE-CHECK GAME ID AND MULTIPLE KWARGS
   photo_file = request.FILES.get('photo-file', None)
-  
-  print(request.user.id)
+  print( type(photo_file), 'photo_file type')
+  # print(request.user.id)
   if photo_file:
-    photo_obj = gpsphoto.getGPSData('photo_file')
-    print('photo_obj:', photo_obj)
+    # photo_obj = gpsphoto.getGPSData('/Users/Shawn/code/where_dis/photo_file')
+    
+    # photo_file_copy = dict(photo_file)
+    photo_file_copy = copy.deepcopy(photo_file)
+    # img_path = save_picture(temp_photo)
+    
+    
+    img = Image.open(photo_file)
+    # img = img.load()
+    
+    # photo_obj = gpsphoto.getGPSData(img_path)
+    # print('photo_obj:', photo_obj)
+    
+    exif_data = img._getexif()
+    # gps_dict = {}
+    print(exif_data, '<=========exif_data')
+    
+    if exif_data is not None:
+      for key, value in exif_data.items():
+        name = TAGS.get(key, key)
+        exif_data[name] = exif_data.pop(key)
+        # print(name, '<=====name')
+      
+      if 'GPSInfo' in exif_data:
+        for key in exif_data['GPSInfo'].keys():
+          name = GPSTAGS.get(key,key)
+          exif_data['GPSInfo'][name] = exif_data['GPSInfo'].pop(key)
+    
+    # print( exif_data['GPSInfo'] )
+    print( exif_data, '<============exif_data' )
+    print( 'GPS Latitude: ', exif_data['GPSInfo']['GPSLatitude'])
+    print( 'GPS Longitude: ', exif_data['GPSInfo']['GPSLongitude'])
+    
+    
+    # print(dir(photo_file))
+    # photo_obj = gpsphoto.getGPSData(f'{sys.path.append(os.path.realpath("photo_file"))}')
+    # print( os.path.abspath("photo_file") )   # /Users/Shawn/code/where_dis/photo_file
+    # print('photo_obj:', photo_obj)
     s3 = boto3.client('s3')
-    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    key = uuid.uuid4().hex[:6] + photo_file_copy.name[photo_file_copy.name.rfind('.'):]
     
     try:
       print('TRY step 1')
-      s3.upload_fileobj(photo_file, BUCKET, key)
+      s3.upload_fileobj(photo_file_copy, BUCKET, key)
       print('TRY step 2')
       url = f'{S3_BASE_URL}{BUCKET}/{key}'
+      # photo_obj = gpsphoto.getGPSData(f'{url}')
+      # print('photo_obj:', photo_obj)
       print(f'TRY step 3 url = {url}')
       photo = Photo(url=url, game_instance_id=game_id, user=request.user) # DOUBLE-CHECK HERE TOO
       print(f'TRY step 4 photo = {photo}')
       photo.save()
       print('TRY step 5')
+      # photo_obj = gpsphoto.getGPSData(f'{url}')
+      # print('photo_obj:', photo_obj)
     except:
       print('There has been an error uploading to S3')
   return redirect('game_detail', game_id=game_id)
+
+
+
+
+
+
+# 34853 in exifdata references a dictionary of coordinates
