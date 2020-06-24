8e5e95728183135eadcc61f3dc388e68850f6c18
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-06-24 07:27:14 -0600

    refactors models

--- main_app/models.py
+++ main_app/models.py
@@ -10,11 +10,31 @@ class GameInstance(models.Model):
     name = models.CharField(max_length=50)
     start_time = models.TimeField(
         auto_now_add=True,  # automatically sets to the time when the object is created
-        )
-    # WINNING LOCATION FROM METADATA FIELD
-    # PLAYERS FIELD MANY TO MANY 
+    )
     # HOST FOREIGN KEY FOREIGN KEY
     # WINNER FOREIGN KEY  NULL=TRUE
+    # WINNING LOCATION FROM METADATA FIELD
+    host = models.ForeignKey(
+        User, 
+        editable=False,             # We shouldn't be able to change the host, select User at creation
+        on_delete=models.CASCADE    # Reconsider? Do we want games to disappear if accounts are deleted
+    )  
+    winner = models.ForeignKey(
+        User, 
+        null=True,                  # Allows nulls in DB
+        blank=True,                 # Allows blank values, UPDATE to user with first winning photo
+        related_name='winner',      # Needed to override conflict with host model, both would have same Django-created name for queries
+        on_delete=models.CASCADE    # Reconsider? Do we want games to disappear if accounts are deleted
+    ) 
+    
+    reference_lat = models.DecimalField(decimal_places=8, max_digits=11, null=True, blank=True) # Blanks b/c game created before photo
+    reference_lng = models.DecimalField(decimal_places=8, max_digits=12, null=True, blank=True)
+    
+    
+    
+    #---------------ICEBOX-----------------#
+    # ENDTIME TO TRACK HOW FAST EACH GAME IS WON
+    # PLAYERS FIELD MANY TO MANY FOR PRIVATE GAMES
     # DIFFICULTY SETTING FIELD
     
     def __str__(self):
@@ -32,8 +52,9 @@ class GameInstance(models.Model):
 class Photo(models.Model):
     url = models.CharField(max_length=200)
     game_instance = models.ForeignKey(GameInstance, on_delete=models.CASCADE)
+    is_reference = models.BooleanField(default=False)           # True indicates the 'goalpost' photo
     user = models.ForeignKey(User, on_delete=models.CASCADE)    # MAY WANT TO RECONSIDER CASCADE HERE. CAN USERS BE DELETED?
-    lat = models.DecimalField(decimal_places=8, max_digits=11)
+    lat = models.DecimalField(decimal_places=8, max_digits=11)  # Do we need some sort of error handling here in the long term?
     lng = models.DecimalField(decimal_places=8, max_digits=12)
     
     def __str__(self):
