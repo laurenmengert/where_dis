4d16ec4f30192dcf350aebd941d99578f49b1c00
Lauren Mengert <laurenmengert@Laurens-MacBook-Pro.local>
2020-06-26 16:18:53 -0600

    Delete comments

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ from exiffield.fields import ExifField
 from GPSPhoto import gpsphoto
 import datetime
 
-# Create your models here.
+
 class GameInstance(models.Model):
     name = models.CharField(max_length=50)
     details = models.TextField(
@@ -13,34 +13,27 @@ class GameInstance(models.Model):
         null=True,
         blank=True
     )
+    
     start_time = models.TimeField(
         auto_now_add=True,  # automatically sets to the time when the object is created
     )
-    # HOST FOREIGN KEY FOREIGN KEY
-    # WINNER FOREIGN KEY  NULL=TRUE
-    # WINNING LOCATION FROM METADATA FIELD
+    
     host = models.ForeignKey(
         User, 
-        editable=False,             # We shouldn't be able to change the host, select User at creation
-        on_delete=models.CASCADE    # Reconsider? Do we want games to disappear if accounts are deleted
+        editable=False,            
+        on_delete=models.CASCADE    
     )  
+    
     winner = models.ForeignKey(
         User, 
         null=True,                  # Allows nulls in DB
         blank=True,                 # Allows blank values, UPDATE to user with first winning photo
-        related_name='winner',      # Needed to override conflict with host model, both would have same Django-created name for queries
-        on_delete=models.CASCADE    # Reconsider? Do we want games to disappear if accounts are deleted
+        related_name='winner',     
+        on_delete=models.CASCADE    
     ) 
     
     reference_lat = models.DecimalField(decimal_places=8, max_digits=11, null=True, blank=True) # Blanks b/c game created before photo
-    reference_lng = models.DecimalField(decimal_places=8, max_digits=12, null=True, blank=True) # Proper deployment will hide these
-    
-    
-    
-    #---------------ICEBOX-----------------#
-    # ENDTIME TO TRACK HOW FAST EACH GAME IS WON
-    # PLAYERS FIELD MANY TO MANY FOR PRIVATE GAMES
-    # DIFFICULTY SETTING FIELD
+    reference_lng = models.DecimalField(decimal_places=8, max_digits=12, null=True, blank=True) 
     
     def __str__(self):
         return self.name
@@ -49,26 +42,14 @@ class GameInstance(models.Model):
         return reverse('game_detail', kwargs={"pk": self.id})
     
     
-# PHOTO MODEL
-    # GAME_INSTANCE_ID FOREIGN KEY FIELD
-    # LOCATION METADATA FIELD
-    # USER_ID FOREIGN KEY FIELD
-    
 class Photo(models.Model):
     url = models.CharField(max_length=200)
     game_instance = models.ForeignKey(GameInstance, on_delete=models.CASCADE)
     is_reference = models.BooleanField(default=False)           # True indicates the 'goalpost' photo
-    user = models.ForeignKey(User, on_delete=models.CASCADE)    # MAY WANT TO RECONSIDER CASCADE HERE. CAN USERS BE DELETED?
-    lat = models.DecimalField(decimal_places=8, max_digits=11)  # Do we need some sort of error handling here in the long term?
+    user = models.ForeignKey(User, on_delete=models.CASCADE)   
+    lat = models.DecimalField(decimal_places=8, max_digits=11)  
     lng = models.DecimalField(decimal_places=8, max_digits=12)
     
     def __str__(self):
         return f'Photo for game_id: {self.game_instance.id} user {self.user_id} @{self.url}'
-    
-
-# USER_INFO MODEL
-    # USER_ID FOREIGN KEY FIELD
-    # IS_HOST BOOLEAN FIELD  -----------  MIGHT BELONG ON GAME MODEL
-    # ROUNDS_WON INTEGER FIELD
-    
     
--- main_app/views.py
+++ main_app/views.py
@@ -18,14 +18,10 @@ import random
 
 # ----------------------CONSTANTS-------------------------- #
 
-
 S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
 BUCKET = 'where-dis'
 
-
-
 # -----------------------GENERAL--------------------------- #
-
 
 def home(request):
   return render(request, 'home.html')
@@ -61,9 +57,8 @@ class GameCreate(LoginRequiredMixin, CreateView):
     return super().form_valid(form)
   
   def get_success_url(self):
-    print(self.object.id, '<====================================================self.object.id')
-    print(type(self.object.id), '<====================================================type(self.object.id)')
     return reverse('game_ref_photo_form', kwargs={'game_id':self.object.id})
+  
   
 class GameDelete(LoginRequiredMixin, DeleteView):
   model = GameInstance
@@ -86,7 +81,6 @@ def game_detail(request, game_id):
   photo_attempts = Photo.objects.filter(game_instance=game_from_db, is_reference=False)
   rand_lat = Decimal(random.uniform(-.0035, .0035)) + game_from_db.reference_lat
   rand_lng = Decimal(random.uniform(-.0035, .0035)) + game_from_db.reference_lng
-  print(ref_photo, '<=======================our photo')
   return render(request, 'game/detail.html', {
     'game': game_from_db,
     'ref_photo': ref_photo,
@@ -95,20 +89,15 @@ def game_detail(request, game_id):
     'rand_lng': rand_lng
   })
 
-# def game_map(request):
-#     mapbox_access_token = ''
-#     return render(request, 'map.html', { 'mapbox_access_token': mapbox_access_token })
+
 @login_required
 def game_map(request, game_id):
   # NEED TO PASS CENTER OF MAP DATA HERE
   context = {'mapbox_access_token': ''}
-  
   return redirect('game_detail', context, game_id=game_id)
 
 
-
 # ------------------------PHOTOS---------------------------- #
-
 
 # Helper function for converting type of extracted lat and long
 def get_decimal_coordinates(info):
@@ -124,6 +113,7 @@ def get_decimal_coordinates(info):
   if 'Latitude' in info and 'Longitude' in info:
       return [info['Latitude'], info['Longitude']]
 
+
 @login_required
 def game_ref_photo_form(request, game_id):
   game = GameInstance.objects.get(id=game_id)
@@ -133,18 +123,18 @@ def game_ref_photo_form(request, game_id):
   }
   return render(request, 'game/ref_photo_form.html', context)
 
+
 @login_required
 def upload_ref_photo_function(request, game_id):
   photo_file = request.FILES.get('photo-file', None)
   game = GameInstance.objects.get(id=game_id)
-  
   
   if photo_file:
     # COPIES PHOTO TO EXTRACT METADATA
     # EXTRACTED PHOTO IS CORRUPTED
     photo_copy = copy.deepcopy(photo_file)
     exif = Image.open(photo_copy)._getexif()
-    print(exif is not None)
+    
     if exif is not None:
         for key, value in exif.items():
             name = TAGS.get(key, key)
@@ -156,92 +146,32 @@ def upload_ref_photo_function(request, game_id):
                 exif['GPSInfo'][name] = exif['GPSInfo'].pop(key)
         else:
           return redirect('game_ref_photo_form', game_id=game_id)
-                
-    print(exif, 'EXIFFFFFFFF')
     decimals = get_decimal_coordinates(exif['GPSInfo'])
-    print(decimals)
-    
     lat = decimals[0]
     lng = decimals[1]
-    
     # SETS WINNING POSITION OF GAME
     game.reference_lat = lat
     game.reference_lng = lng
     game.save()
-    
     # SAVES PHOTO TO S3, AND SAVES REFS TO SQL DB
     s3 = boto3.client('s3')
     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-    
     try:
-      print('TRY step 1')
       s3.upload_fileobj(photo_file, BUCKET, key)
-      print('TRY step 2')
       url = f'{S3_BASE_URL}{BUCKET}/{key}'
-      # photo_obj = gpsphoto.getGPSData(f'{url}')
-      # print('photo_obj:', photo_obj)
-      print(f'TRY step 3 url = {url}')
-      photo = Photo(url=url, game_instance_id=game_id, user=request.user, lat=lat, lng=lng, is_reference=True) # DOUBLE-CHECK HERE TOO
-      print(f'TRY step 4 photo = {photo}')
+      photo = Photo(url=url, game_instance_id=game_id, user=request.user, lat=lat, lng=lng, is_reference=True)
       photo.save()
-      print('TRY step 5')
-      # photo_obj = gpsphoto.getGPSData(f'{url}')
-      # print('photo_obj:', photo_obj)
     except:
       print('There has been an error uploading to S3')
     return redirect('game_detail', game_id=game_id)
-  
   else:
     return redirect('game_ref_photo_form', game_id=game_id)
-
-
-    
-
-# def save_picture(form_picture):
-#     # this function has to do with the module Pillow
-#     # PIL
-#     # purpose is to save the image as a static asset
-#     # 1. generate a random name
-#     random_hex = uuid.uuid4().hex[:8]# generates a random integer
-#     # grabbing the ext , jpeg, jpg
-    
-#     print(form_picture, '<=====form_picture')
-#     f_name, f_ext = os.path.splitext(form_picture.name)
-#     # => ['jimProfile', 'png']
-#     # create the random picture name with the correct extension
-#     picture_name = random_hex + f_ext
-#     # create the file_path
-#     file_path = os.path.join(os.getcwd(), 'main_app/tmp/' + picture_name)
-#     print(file_path, '<=======================file_path')
-    
-#     # file_path = os.path.join(os.getcwd(), 'main_app/tmp' + picture_name) # print this
-#     # Pillow code stars
-#     # output_size = (125, 175) # set the size of picture, as tuple
-#     # open the file sent from the client
-#     i = Image.open(form_picture)
-#     # exif = i._exif._loaded_exif
-#     # print( i.gps_latitude, 'latitude')
-    
-#     exif_dict = piexif.load(i.info["exif"])
-#     exif_bytes = piexif.dump(exif_dict)
-    
-#     print(exif_bytes, '<=======exif_bytes')
-    
-#     # print(exif, 'exifffffffffffffffffffffffffffff')
-    
-#     # i.thumbnail(output_size) # set the size accepts a tuple with dimensions
-#     i.save(file_path) # save it to file path we created
-#     return file_path  #RETURN FILEPATH
-  
-  
-# # CREATE HELPER FUNCTION TO DELETE
 
 
 # THIS FUNCTION IS ONLY CALLED ON NON-REFERENCE PHOTOS FOR A GAME
 @login_required
 def upload_photo(request, game_id): # DOUBLE-CHECK GAME ID AND MULTIPLE KWARGS
   photo_file = request.FILES.get('photo-file', None)
-  
   
   if photo_file:
     # COPIES PHOTO TO EXTRACT METADATA
@@ -253,56 +183,31 @@ def upload_photo(request, game_id): # DOUBLE-CHECK GAME ID AND MULTIPLE KWARGS
         for key, value in exif.items():
             name = TAGS.get(key, key)
             exif[name] = exif.pop(key)
-
         if 'GPSInfo' in exif:
             for key in exif['GPSInfo'].keys():
                 name = GPSTAGS.get(key,key)
-                exif['GPSInfo'][name] = exif['GPSInfo'].pop(key)
-                
-    print(exif, 'EXIFFFFFFFF')
+                exif['GPSInfo'][name] = exif['GPSInfo'].pop(key)         
     decimals = get_decimal_coordinates(exif['GPSInfo'])
     print(decimals)
     
     lat = Decimal(decimals[0])
     lng = Decimal(decimals[1])
     
-    
-    
-    
-    
     # SAVES PHOTO TO S3, AND SAVES REFS TO SQL DB
     s3 = boto3.client('s3')
     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
     game = GameInstance.objects.get(id=game_id)
-    print(game, '<================game')
-    print(game.winner, '<=================game.winner')
-    
     try:
-      print('TRY step 1')
       s3.upload_fileobj(photo_file, BUCKET, key)
-      print('TRY step 2')
       url = f'{S3_BASE_URL}{BUCKET}/{key}'
-      # photo_obj = gpsphoto.getGPSData(f'{url}')
-      # print('photo_obj:', photo_obj)
-      print(f'TRY step 3 url = {url}')
-      print(request.user, '<===============request.user')
-      photo = Photo(url=url, game_instance_id=game_id, user=request.user, lat=lat, lng=lng) # DOUBLE-CHECK HERE TOO
+      photo = Photo(url=url, game_instance_id=game_id, user=request.user, lat=lat, lng=lng)
       # WIN LOGIC
       # 0.00001 is appx 1 meter, so 0.0004 is somewhere around 12 feet
       # so this creates about a 24ft x 24ft box as a margin of error
-      print(type(lat), '<===============type(lat)')
-      print(type(game.reference_lat), '<===============type(game.reference_lat)')
-      print(abs(lat - game.reference_lat), '<============absolute difference lats ')
       if (abs(lat - game.reference_lat) < 0.00008) and (abs(lng - game.reference_lng) < 0.00008):
-        # print(photo.user, '<=======================photo.user')
-        # DOES THIS UPDATE GAME_INSTANCE.WINNER PROPERLY??!!??!??
         game.winner = request.user
         game.save()
-      print(f'TRY step 4 photo = {photo}')
       photo.save()
-      print('TRY step 5')
-      # photo_obj = gpsphoto.getGPSData(f'{url}')
-      # print('photo_obj:', photo_obj)
     except:
       print('There has been an error uploading to S3')
   return redirect('game_detail', game_id=game_id)
